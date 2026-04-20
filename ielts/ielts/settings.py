41557756b84804b6f92d18eeb5a59a5729b19fb3
abduhokimmessi10-wq <abduhokimmessi10@gmail.com>
2026-04-20 20:23:54 +0500

    fix databases

--- ielts/ielts/settings.py
+++ ielts/ielts/settings.py
@@ -1,7 +1,8 @@
-import os
+
 from pathlib import Path
 from dotenv import load_dotenv
-
+import dj_database_url
+import os
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,15 +56,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ielts.wsgi.application'
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'ielts_db'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 REST_FRAMEWORK = {
