8ba99f4b9e694af2883924412c244f61f15fe741
abduhokimmessi10-wq <abduhokimmessi10@gmail.com>
2026-04-21 17:51:02 +0500

    fix databases

--- ielts/ielts/urls.py
+++ ielts/ielts/urls.py
@@ -7,3 +7,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('speaking.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
+
+User = get_user_model()
+try:
+    # Bu yerga o'zingiz xohlagan login va parolni yozing
+    User.objects.create_superuser('nakata', 'nakata@example.com', 'nakata')
+    print("Superuser muvaffaqiyatli yaratildi!")
+except IntegrityError:
+    print("Superuser allaqachon mavjud.")
+except Exception as e:
+    print(f"Xato: {e}")
